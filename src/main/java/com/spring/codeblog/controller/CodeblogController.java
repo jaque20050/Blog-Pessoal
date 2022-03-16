be1daf9477bf798a7b33c58a8b9f6b81ec9687ac
Jaqueline Camilo Dias <jaque20050@hotmail.com>
2022-03-16 14:33:25 -0300

    Criando o Controller

---
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -0,0 +1,27 @@
+package com.spring.codeblog.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.service.codeblogService;
+
+@Controller
+public class CodeblogController {
+
+	@Autowired
+	codeblogService codeblogService;
+
+    @RequestMapping(value="/posts", method=RequestMethod.GET)
+    public ModelAndView getPosts(){
+        ModelAndView mv = new ModelAndView("posts");
+        List<Post> posts = codeblogService.findAll();
+        mv.addObject("posts", posts);
+        return mv;
+    }
+}
